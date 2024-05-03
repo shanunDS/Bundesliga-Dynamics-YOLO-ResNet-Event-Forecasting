49a02e62562864eea6e0275a9c35b895d98dad3d
Shanun Randev <143093344+shanunDS@users.noreply.github.com>
2024-05-03 18:38:13 -0400

    Update main_final.py

--- main_final.py
+++ main_final.py
@@ -5,34 +5,38 @@ import numpy as np
 import pandas as pd
 import cv2
 import random
-
-
+import matplotlib.pyplot as plt
+from tqdm.notebook import tqdm
 import glob
 from sklearn.metrics import confusion_matrix
 import copy
 
 
-
-IMSIZE = [540, 768]
-IMG_SIZE = (540, 768)
+IMG_SIZE = (640, 768)
+# modelname = "tf_efficientnet_b0"
 use_amp = True
 batch_size = 40
-n_epochs = 30
+n_epochs = 5
 num_workers = 2
 COSINE = True
 init_lr = 1e-4
-
-IMG_SOURCE = "img"
+# IMG_SOURCE = "img"
 BACK_INTERVAL = 20
 BACK_INTERVAL_VAL = 1
 ERR_TOL = 1
 mixup = True
 
 
+selected_videos = [
+    "1606b0e6_0.mp4",
+    "35bd9041_0.mp4"
+    # "3c993bd2_0.mp4"
+    # '35bd9041_1.mp4',
+    # '407c5a9e_1.mp4'
+
+]
 
 
-
-event_names = ['challenge', 'throwin', 'play']
 
 
 err_tol = {
@@ -44,11 +48,19 @@ err_tol = {
 
 
 
+
+event_names = ['challenge', 'throwin', 'play']
+
+df = pd.read_csv("/home/ubuntu/bundesliga/src/Data/train.csv")
+
 # df = pd.read_csv("/home/ubuntu/DL/src/Data/train.csv")
 
 
-df = pd.read_csv("/home/ubuntu/bundesliga/src/Data/train.csv")
 print(len(df))
+
+print(df.head(15))
+
+
 additional_events = []
 for arr in df.sort_values(['video_id', 'time', 'event', 'event_attributes']).values:
     if arr[2] in err_tol:
@@ -67,16 +79,124 @@ df = df[~df['event'].isin(event_names)]
 df = df.sort_values(['video_id', 'time'])
 
 
+print('main dataset')
+
+print(df.head())
+
+
+
+
 
 
 cap = cv2.VideoCapture("/home/ubuntu/bundesliga/src/Data/train/3c993bd2_0.mp4")
+# cap = cv2.VideoCapture("/home/ubuntu/DL/src/Data/train/3c993bd2_0.mp4")
+
+
 fps = cap.get(cv2.CAP_PROP_FPS)
+
 print("fps:", fps)
 df["frame"] = df["time"] * fps
-
 
 print(df.head(10))
 
 
-df.to_csv('/home/ubuntu/bundesliga/src/Data/updated_frames_new.csv', index=False)
+
+
+
+def extract_images(video_path, out_dir):
+    video_name = os.path.basename(video_path).split('.')[0]
+    cam = cv2.VideoCapture(video_path)
+    print(video_path)
+    frame_count = 1
+    while True:
+        successed, img = cam.read()
+        if not successed:
+            break
+        print('output_img_file')
+        outfile = f'{out_dir}/{video_name}-{frame_count:06}.jpg'
+        print(outfile)
+        # print('img')
+        img = cv2.resize(img, dsize=IMG_SIZE, interpolation=cv2.INTER_AREA)
+        # print(img)
+        cv2.imwrite(outfile, img)
+        frame_count += 1
+
+#
+#
+# def extract_images(video_path, out_dir, frame_interval=100):
+#     video_name = os.path.basename(video_path).split('.')[0]
+#     cam = cv2.VideoCapture(video_path)
+#     print(video_path)
+#     frame_count = 1
+#     while True:
+#         successed, img = cam.read()
+#         if not successed:
+#             break
+#         if frame_count % frame_interval == 0:  # Extract frames at specified interval
+#             print('output_img_file')
+#             outfile = f'{out_dir}/{video_name}-{frame_count:06}.jpg'
+#             print(outfile)
+#             img = cv2.resize(img, dsize=IMG_SIZE, interpolation=cv2.INTER_AREA)
+#             cv2.imwrite(outfile, img)
+#         frame_count += 1
+
+
+
+
+
+# Path to the directory containing the MP4 files
+directory = "/home/ubuntu/bundesliga/src/Data/train"
+
+# directory = "/home/ubuntu/DL/src/Data/train"
+
+
+
+OUT_DIR = "/home/ubuntu/bundesliga/src/work"
+# OUT_DIR = "home/ubuntu/DL/src/work"
+
+
+train_out_dir = os.path.join(OUT_DIR, "img_train")
+val_out_dir = os.path.join(OUT_DIR, "img_val")
+
+
+os.makedirs(train_out_dir, exist_ok=True)
+os.makedirs(val_out_dir, exist_ok=True)
+
+random.shuffle(selected_videos)
+
+
+split_ratio = 0.8
+
+# Split the videos into training and validation sets
+split_index = int(len(selected_videos) * split_ratio)
+
+print('train videos')
+train_videos = selected_videos[:split_index]
+
+print(train_videos)
+
+print('validation videos')
+valid_videos = selected_videos[split_index:]
+print(valid_videos)
+
+# Extract images from training videos
+for video_file in train_videos:
+    video_path = os.path.join(directory, video_file)
+    extract_images(video_path, train_out_dir)
+
+# Extract images from validation videos
+for video_file in valid_videos:
+    video_path = os.path.join(directory, video_file)
+    extract_images(video_path, val_out_dir)
+
+
+
+
+
+
+
+
+
+
+
 
