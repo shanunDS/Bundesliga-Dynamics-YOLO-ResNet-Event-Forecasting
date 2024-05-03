a3201aba1deb5ecbb66026e95bf958f7795e4df0
KalyaniVinayagamSivasundari <kalyani.vinayagamsivasundari@gwmail.gwu.edu>
2024-05-03 23:26:28 +0000

    update

--- main.py
+++ main.py
@@ -9,68 +9,9 @@ from player_ball_assigner import PlayerBallAssigner
 import streamlit as st
 from io import BytesIO
 
-# def main():
-#     # Read Video
-#     video_frames = read_video('clips/08fd33_4.mp4')
-#
-#     # Initialize Tracker
-#     tracker = Tracker('models/best.pt')
-#
-#     tracks = tracker.get_object_tracks(video_frames,
-#                                        read_from_stub=True,
-#                                        stub_path='stubs/track_stubs.pkl')
-#
-#     # Interpolate Ball Positions
-#     tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
-#
-#     print("Tracker")
-#     print(tracks["ball"])
-#
-#
-#     # Assign Player Teams
-#     team_assigner = TeamAssigner()
-#     team_assigner.assign_team_color(video_frames[0],
-#                                     tracks['players'][0])
-#
-#     for frame_num, player_track in enumerate(tracks['players']):
-#         for player_id, track in player_track.items():
-#             team = team_assigner.get_player_team(video_frames[frame_num],
-#                                                  track['bbox'],
-#                                                  player_id)
-#             tracks['players'][frame_num][player_id]['team'] = team
-#             tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
-#
-#
-#
-#     # Assign Ball Aquisition
-#     player_assigner = PlayerBallAssigner()
-#     team_ball_control = []
-#     for frame_num, player_track in enumerate(tracks['players']):
-#         ball_bbox = tracks['ball'][frame_num][1]['bbox']
-#         assigned_player = player_assigner.assign_ball_to_player(player_track, ball_bbox)
-#
-#         if assigned_player != -1:
-#             tracks['players'][frame_num][assigned_player]['has_ball'] = True
-#             team_ball_control.append(tracks['players'][frame_num][assigned_player]['team'])
-#         else:
-#             team_ball_control.append(team_ball_control[-1])
-#     team_ball_control = np.array(team_ball_control)
-#
-#
-#     # Draw output
-#     ## Draw object Tracks
-#     output_video_frames = tracker.draw_annotations(video_frames, tracks, team_ball_control)
-#
-#
-#
-#     # Save Video
-#     save_video(output_video_frames, 'clips_output/output.avi')
-
-
-
-def process_video(video_path):
+def main():
     # Read Video
-    video_frames = read_video(video_path)
+    video_frames = read_video('clips/08fd33_4.mp4')
 
     # Initialize Tracker
     tracker = Tracker('models/best.pt')
@@ -82,9 +23,14 @@ def process_video(video_path):
     # Interpolate Ball Positions
     tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
 
+    print("Tracker")
+    print(tracks["ball"])
+
+
     # Assign Player Teams
     team_assigner = TeamAssigner()
-    team_assigner.assign_team_color(video_frames[0], tracks['players'][0])
+    team_assigner.assign_team_color(video_frames[0],
+                                    tracks['players'][0])
 
     for frame_num, player_track in enumerate(tracks['players']):
         for player_id, track in player_track.items():
@@ -94,7 +40,9 @@ def process_video(video_path):
             tracks['players'][frame_num][player_id]['team'] = team
             tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
 
-    # Assign Ball Acquisition
+
+
+    # Assign Ball Aquisition
     player_assigner = PlayerBallAssigner()
     team_ball_control = []
     for frame_num, player_track in enumerate(tracks['players']):
@@ -108,39 +56,91 @@ def process_video(video_path):
             team_ball_control.append(team_ball_control[-1])
     team_ball_control = np.array(team_ball_control)
 
+
     # Draw output
     ## Draw object Tracks
     output_video_frames = tracker.draw_annotations(video_frames, tracks, team_ball_control)
 
-    return output_video_frames
 
 
-def main():
-    st.title("Video Tracking App")
+    # Save Video
+    save_video(output_video_frames, 'clips_output/output.avi')
 
-    # File uploader
-    uploaded_file = st.file_uploader("Upload a video file", type=["mp4"])
 
-    if uploaded_file is not None:
-        st.text("Processing... This might take a while depending on the video length.")
 
-        uploaded_file = "C:/Users/kalya/OneDrive/Desktop/Project_DL/DL/clips/08fd33_4.mp4"
-
-        # Process the video
-        output_frames = process_video(uploaded_file)
-
-        output_buffer = BytesIO()
-        save_video(output_frames, output_buffer)
-
-        # Provide download link
-        st.text("Video processed successfully. You can download the output file below.")
-        st.download_button(label="Download Output Video", data=output_buffer.getvalue(), file_name="output123")
-
-        # Save Video
-        # save_video(output_frames, 'C:/Users/kalya/OneDrive/Desktop/Project_DL/DL/clips_output/output.avi')
-        #
-        # st.text("Video processed successfully. You can download the output file below.")
-        # st.download_button(label="Download Output Video", data='C:/Users/kalya/OneDrive/Desktop/Project_DL/DL/clips_output/', file_name="output123.avi")
+# def process_video(video_path):
+#     # Read Video
+#     video_frames = read_video(video_path)
+#
+#     # Initialize Tracker
+#     tracker = Tracker('models/best.pt')
+#
+#     tracks = tracker.get_object_tracks(video_frames,
+#                                        read_from_stub=True,
+#                                        stub_path='stubs/track_stubs.pkl')
+#
+#     # Interpolate Ball Positions
+#     tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
+#
+#     # Assign Player Teams
+#     team_assigner = TeamAssigner()
+#     team_assigner.assign_team_color(video_frames[0], tracks['players'][0])
+#
+#     for frame_num, player_track in enumerate(tracks['players']):
+#         for player_id, track in player_track.items():
+#             team = team_assigner.get_player_team(video_frames[frame_num],
+#                                                  track['bbox'],
+#                                                  player_id)
+#             tracks['players'][frame_num][player_id]['team'] = team
+#             tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+#
+#     # Assign Ball Acquisition
+#     player_assigner = PlayerBallAssigner()
+#     team_ball_control = []
+#     for frame_num, player_track in enumerate(tracks['players']):
+#         ball_bbox = tracks['ball'][frame_num][1]['bbox']
+#         assigned_player = player_assigner.assign_ball_to_player(player_track, ball_bbox)
+#
+#         if assigned_player != -1:
+#             tracks['players'][frame_num][assigned_player]['has_ball'] = True
+#             team_ball_control.append(tracks['players'][frame_num][assigned_player]['team'])
+#         else:
+#             team_ball_control.append(team_ball_control[-1])
+#     team_ball_control = np.array(team_ball_control)
+#
+#     # Draw output
+#     ## Draw object Tracks
+#     output_video_frames = tracker.draw_annotations(video_frames, tracks, team_ball_control)
+#
+#     return output_video_frames
+#
+#
+# def main():
+#     st.title("Video Tracking App")
+#
+#     # File uploader
+#     uploaded_file = st.file_uploader("Upload a video file", type=["mp4"])
+#
+#     if uploaded_file is not None:
+#         st.text("Processing... This might take a while depending on the video length.")
+#
+#         uploaded_file = "C:/Users/kalya/OneDrive/Desktop/Project_DL/DL/clips/08fd33_4.mp4"
+#
+#         # Process the video
+#         output_frames = process_video(uploaded_file)
+#
+#         output_buffer = BytesIO()
+#         save_video(output_frames, output_buffer)
+#
+#         # Provide download link
+#         st.text("Video processed successfully. You can download the output file below.")
+#         st.download_button(label="Download Output Video", data=output_buffer.getvalue(), file_name="output123")
+#
+#         # Save Video
+#         # save_video(output_frames, 'C:/Users/kalya/OneDrive/Desktop/Project_DL/DL/clips_output/output.avi')
+#         #
+#         # st.text("Video processed successfully. You can download the output file below.")
+#         # st.download_button(label="Download Output Video", data='C:/Users/kalya/OneDrive/Desktop/Project_DL/DL/clips_output/', file_name="output123.avi")
 
 if __name__ == "__main__":
     main()
--- utils/video_utils.py
+++ utils/video_utils.py
@@ -13,42 +13,42 @@ def read_video(video_path):
         frames.append(frame)
     return frames
 
-# def save_video(output_video_frames, output_video_path):
-#     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-#     out = cv2.VideoWriter(output_video_path, fourcc, 24, (output_video_frames[0].shape[1], output_video_frames[0].shape[0]))
-#     for frame in output_video_frames:
-#         out.write(frame)
-#     out.release()
+def save_video(output_video_frames, output_video_path):
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(output_video_path, fourcc, 24, (output_video_frames[0].shape[1], output_video_frames[0].shape[0]))
+    for frame in output_video_frames:
+        out.write(frame)
+    out.release()
 
-# def save_video(output_video_frames, output_buffer):
-#     frames = []
-#     for frame in output_video_frames:
-#         # Encode the frame as a JPEG image
-#         success, encoded_frame = cv2.imencode('.jpg', frame)
-#         if success:
-#             frames.append(encoded_frame.tobytes())
-#         else:
-#             print("Failed to encode frame")
+# # def save_video(output_video_frames, output_buffer):
+# #     frames = []
+# #     for frame in output_video_frames:
+# #         # Encode the frame as a JPEG image
+# #         success, encoded_frame = cv2.imencode('.jpg', frame)
+# #         if success:
+# #             frames.append(encoded_frame.tobytes())
+# #         else:
+# #             print("Failed to encode frame")
+# #
+# #     # Concatenate all the encoded frames into a single byte array
+# #     output_buffer.write(b''.join(frames))
 #
-#     # Concatenate all the encoded frames into a single byte array
-#     output_buffer.write(b''.join(frames))
-
-
-def save_video(output_video_frames, output_buffer):
-    # Create a temporary file
-    with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as temp_file:
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        height, width, _ = output_video_frames[0].shape
-        out = cv2.VideoWriter(temp_file.name, fourcc, 24, (width, height))
-
-        for frame in output_video_frames:
-            out.write(frame)
-
-        out.release()
-
-        # Read the temp file into the output buffer
-        temp_file.seek(0)
-        output_buffer.write(temp_file.read())
-
-    # Remove the temporary file
-    os.remove(temp_file.name)
+#
+# def save_video(output_video_frames, output_buffer):
+#     # Create a temporary file
+#     with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as temp_file:
+#         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+#         height, width, _ = output_video_frames[0].shape
+#         out = cv2.VideoWriter(temp_file.name, fourcc, 24, (width, height))
+#
+#         for frame in output_video_frames:
+#             out.write(frame)
+#
+#         out.release()
+#
+#         # Read the temp file into the output buffer
+#         temp_file.seek(0)
+#         output_buffer.write(temp_file.read())
+#
+#     # Remove the temporary file
+#     os.remove(temp_file.name)
